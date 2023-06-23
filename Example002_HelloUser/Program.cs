e68a5827b8c0a45f3ac17b6e8b32c16a2e2de483
DmitriyG <79133850099@yandex.ru>
2023-06-23 17:34:14 +0700

    Example 002 Hello User

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿// Приветствие User
+Console.WriteLine("Введите ваше имя:  ");
+string username = Console.ReadLine();
+Console.WriteLine("  ");
+Console.Write("Привет, ");
+Console.Write(username);
